8329f71bb77f1e6c706d94e500f6225ec2517d15
JunkaiZhang1201 <1051079612@qq.com>
2023-03-15 22:23:32 +1100

    functions: count power, count number, spread

--- search/program.py
+++ search/program.py
@@ -73,3 +73,56 @@ def count_red_num(board):
             total += 1
 
     return total
+
+def update_board(board, target, chess_col):
+
+    # update a exist chess
+    if target in board:
+        chess_cur = board.get(target)
+        cur_pow = chess_cur[1]
+
+        # when current location has a power 6 chess
+        if cur_pow == 6:
+            del board[target]
+        else:
+            next_pow = cur_pow + 1
+            board[target] = (chess_col, next_pow)
+
+    # create a new chess
+    else:
+        board[target] = (chess_col, 1)
+
+# board is a dictionary in form of {(1,1):('r',1),(1,2):('b',2)}
+# action is a tuple in form of (1,1,0,1)
+def spread(board, action):
+    chess_r = action[0]
+    chess_q = action[1]
+    direction_r = action[2]
+    direction_q = action[3]
+    status = board.get(chess_r, chess_q)
+    chess_col = status[0]
+    chess_pow = status[1]
+
+    for i in chess_pow:
+        target_r = chess_r + direction_r
+
+        # when hit the board edge
+        if target_r < 0:
+            target_r = 6
+        elif target_r > 6:
+            target_r = 0
+
+        target_q = chess_q + direction_q
+
+        # when hit the board edge
+        if target_q < 0:
+            target_q = 6
+        elif target_q > 6:
+            target_q = 0
+
+        target = (target_r, target_q)
+        update_board(board, target, chess_col)
+
+
+
+
